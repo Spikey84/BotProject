2597943845a0541b7da2d854cd71a15f31d64f77
Spikey Noob <creativename1111@gmail.com>
2021-07-20 00:34:57 -0400

    Initial commit

---
+++ src/main/java/io/github/spikey84/botproject/Main.java
@@ -0,0 +1,44 @@
+package io.github.spikey84.botproject;
+
+import io.github.spikey84.botproject.listeners.ClearChannelCommand;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import javax.security.auth.login.LoginException;
+
+public class Main extends ListenerAdapter {
+    //only static cus main needs to be and i cannot create this in main without it being static
+
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Please enter a token!");
+            System.exit(1);
+        }
+        String token = args[0];
+
+        JDA jda = null;
+        try {
+            jda = JDABuilder.createDefault(token)
+                    .setActivity(Activity.playing("code code code"))
+                    .addEventListeners(new ClearChannelCommand())
+                    .build();
+            System.out.println("Login Success!");
+        } catch (LoginException e) {
+            System.out.println("Login Failed");
+            System.exit(1);
+        }
+
+        //jda.upsertCommand("announce", "announces given text").queue();
+    }
+//    public static void main(String[] args) throws Exception{
+//        JDABuilder.createDefault(args[0]) // don't use the deprecated constructor
+//                .addEventListeners(new ClearChannelCommand()) // register your listener
+//               .build();
+//    }
+
+}
---
+++ src/main/java/io/github/spikey84/botproject/listeners/ClearChannelCommand.java
@@ -0,0 +1,77 @@
+package io.github.spikey84.botproject.listeners;
+
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.Button;
+import okhttp3.internal.http2.Http2Connection;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ClearChannelCommand extends ListenerAdapter {
+
+    public ClearChannelCommand() {
+        System.out.println("EVENT READYe");
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        System.out.println("Message");
+        if (!event.getMessage().getContentRaw().contains("!clearChannel")) return;
+
+//        if (!event.getMember().getPermissions().contains(Permission.ADMINISTRATOR)) {
+//            event.getMessage().reply("You do not have permission to run this command.").mentionRepliedUser(false)
+//                    .queue();
+//            return;
+//        }
+
+        event.getMessage().reply("Are you sure u want to delete all messages?").mentionRepliedUser(false)
+                .setActionRow(net.dv8tion.jda.api.interactions.components.Button.success("confirm", "confirm"),
+                        Button.danger("deny", "deny"))
+                .queue();
+    }
+
+    @Override
+    public void onButtonClick(ButtonClickEvent event) {
+        if(!event.getMessage().getContentRaw().equals("Are you sure u want to delete all messages?")) return;
+
+        if (!event.getMember().hasPermission(Permission.ADMINISTRATOR)) {
+            event.deferEdit();
+            return;
+        }
+
+        if (event.getComponentId().equals("confirm")) {
+            event.getMessage().getTextChannel().getIterableHistory().queue( messageHistory -> {
+                Message message = messageHistory.get(0);
+                int x = 1;
+                message.delete().queue((result) -> {
+
+                   reDel(x, messageHistory);
+                    System.out.println("del");
+                });
+
+            });
+
+
+
+        } else if (event.getComponentId().equals("deny")) {
+            if (!event.getMember().hasPermission(Permission.ADMINISTRATOR)) {
+                event.deferEdit();
+                return;
+            }
+        }
+    }
+
+    public int reDel(int i, List<Message> mesHis) {
+        if (i == -1) return -1;
+        if (mesHis.size() < i) return -1;
+        mesHis.get(i).delete().queue((result) -> {
+            reDel(i+1, mesHis);
+            System.out.println("del");
+        });
+        return -1;
+    }
+}
